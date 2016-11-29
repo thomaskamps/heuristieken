a911f93bc771c67ab31f5d54e49ed1199d96457d
Martijn66 <martijn.dejong@mail.com>
2016-11-29 18:24:53 +0100

    Heuristic blocking car/truck

--- astarTurbo.py
+++ astarTurbo.py
@@ -50,10 +50,15 @@ def bfs(grid, car_list):
 						# Check if grid has already existed
 						if newGridObjGridStr not in pre_grid:
 						
-							cost = len(newGridObj.grid) - newGridObj.car_list[1][2] - newGridObj.car_list[1][1] + get_grid[0]
+							# Heuristic: Distance red car to exit
+							distancetoboard = len(newGridObj.grid) - newGridObj.car_list[1][2] - newGridObj.car_list[1][1]
+							cost =  distancetoboard + get_grid[0] 
+							
+							# Heuristic: Cost of blocking car is higher than blocking truck
 							for i in range(newGridObj.car_list[1][2] + newGridObj.car_list[1][1], len(newGridObj.grid)):
 								if newGridObj.retrieve_value(i, newGridObj.car_list[1][3]) != 0:
-									cost += 1
+									if newGridObj.car_list[1][1] == 2:
+										cost += 100
 							
 							# Check for solution (clear path to endpoint)
 							if newGridObj.check_solution():
--- testmartijn.py
+++ testmartijn.py
@@ -53,15 +53,21 @@ def bfs(grid, car_list):
 						# Check if grid has already existed
 						if newGridObjGridStr not in pre_grid:
 							
+							# Heuristic: Distance red car to exit
 							distancetoboard = len(newGridObj.grid) - newGridObj.car_list[1][2] - newGridObj.car_list[1][1]
-							cost =  distancetoboard + get_grid[0]
+							cost =  distancetoboard + get_grid[0] 
 							
+							# Heuristic: Cost of blocking car is higher than blocking truck
 							for i in range(newGridObj.car_list[1][2] + newGridObj.car_list[1][1], len(newGridObj.grid)):
 								if newGridObj.retrieve_value(i, newGridObj.car_list[1][3]) != 0:
-									if newGridObj.car_list[1][1] == 3:
-										cost += 1
 									if newGridObj.car_list[1][1] == 2:
-										cost += 10
+										cost += 100
+							
+
+							for i in range(1, len(newGridObj.car_list)):
+								extracost = ((newGridObj.car_list[i][1] + newGridObj.car_list[i][2]) * 10)
+								cost += extracost
+								
 							
 							# Check for solution (clear path to endpoint)
 							if newGridObj.check_solution():
